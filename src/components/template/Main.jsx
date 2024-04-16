98519871c5b243219ed45fde4dc76b62d41f65ac
httpsdfaa <deivisondfaa@gmail.com>
2024-04-15 22:55:58 -0300

    quote and padding in main

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -3,6 +3,7 @@ import './Main.css';
 import IntroBg from './IntroBg';
 import Cards from './Cards';
 import Skills from './Skills';
+//import Quotes from './Quotes';
 
 export default props =>
     <main className="content">
---
+++ src/components/template/Quotes.jsx
@@ -0,0 +1,14 @@
+import './Quotes.css'
+
+export default props =>
+    <section className='qt'>
+        <h2>Bill Gates</h2>
+        <aside>
+            <blockquote>
+                <p className='msg'>
+                    A maneira como você coleta, gerencia e utiliza as informações
+                    determina se você vai verncer ou perder
+                </p>
+            </blockquote>
+        </aside>
+    </section>
