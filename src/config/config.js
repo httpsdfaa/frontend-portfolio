be6bd2373919e0acc0f5a1781c24ec0192cb684d
httpsdfaa <deivisondfaa@gmail.com>
2024-05-15 22:37:03 -0300

    dominio

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
-        url: 'https://backend-deivison.vercel.app/',
+        url: 'https://backend-portfolio-gules.vercel.app/',
         url_test: 'http://localhost:3001/auth/form'
     }
 }
