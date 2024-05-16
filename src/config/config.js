ddb097de91f162765bb7a7532f6fb2d035066d26
httpsdfaa <deivisondfaa@gmail.com>
2024-05-15 22:39:44 -0300

    dominio

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
-        url: 'https://backend-portfolio-gules.vercel.app/',
+        url: 'https://backend-portfolio-gules.vercel.app/auth/form',
         url_test: 'http://localhost:3001/auth/form'
     }
 }
