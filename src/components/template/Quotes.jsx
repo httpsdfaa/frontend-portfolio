0fa698359cf89764e9b76b569e7ce79839cb03ba
Deivison Fernandes <60937902+httpsdfaa@users.noreply.github.com>
2024-05-22 21:38:49 -0300

    Atualizando frase

--- src/components/template/Quotes.jsx
+++ src/components/template/Quotes.jsx
@@ -5,12 +5,12 @@ import LinkedInBadge from './LinkedinSelo'
 export default props =>
     <div className='qt-div'>
         <section className='qt' maxWidth='true'>
-            <h2 className='quote-title'>Albert Einstein</h2>
+            <h2 className='quote-title'>Alan Turing</h2>
             <hr></hr>
             <aside>
                 <blockquote>
                     <p className='msg'>
-                        A definição de insanidade é fazer a mesma coisa repetidamente e esperar resultados diferentes
+                        Those who can imagine anything, can create the impossible.
                     </p>
                 </blockquote>
             </aside>
