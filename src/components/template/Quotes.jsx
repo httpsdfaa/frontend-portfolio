89065ecf1c39fb05644f056fa3f9e6a91e8cf998
Deivison Fernandes <60937902+httpsdfaa@users.noreply.github.com>
2024-07-30 18:24:12 -0300

    Update Quotes.jsx

--- src/components/template/Quotes.jsx
+++ src/components/template/Quotes.jsx
@@ -5,12 +5,12 @@ import LinkedInBadge from './LinkedinSelo'
 export default props =>
     <div className='qt-div'>
         <section className='qt' maxWidth='true'>
-            <h2 className='quote-title'>Sean Rad</h2>
+            <h2 className='quote-title'>Sherlock Holmes</h2>
             <hr></hr>
             <aside>
                 <blockquote>
                     <p className='msg'>
-                        Os dados batem as emoções.
+                        Dados! Dados! Preciso de dados! Não posso fazer tijolos sem barro!.
                     </p>
                 </blockquote>
             </aside>
